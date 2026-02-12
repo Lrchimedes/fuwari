5340642cc14dccbc9911c8b198b09bd01da910e8
Lrchimedes <lrchimedes@gmail.com>
2026-02-12 21:37:47 +0800

    项目初始化

--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-export function onRequest(context) {
-	const url = new URL(context.request.url);
-	const origin = `${url.protocol}//${url.host}`;
-	
-	context.locals.siteUrl = origin;
-	
-	return context.next();
-}
